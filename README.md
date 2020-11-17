314f3fc4d3cffae977fbf260ac81bc5a5ca5c27d
Nahuel Oyhanarte <noyhanarte@gmail.com>
2020-11-17 19:20:47 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,88 @@
+# TP Teoría de Control - UTN - FRBA
+
+Sistema de Control de Riego.
+
+## Integrantes
+
+Nombre | Legajo | Mail |
+:---: | :---: | :---: |
+Hernán Domingo | 1306315 | hernan.domingo.22@gmail.com |
+Nahuel Oyhanarte | 1545668 | noyhanarte@gmail.com |
+De Luca Rodrigo | 1398143 | cdelucagallego@est.frba.utn.edu.ar | 
+Constenla Juan Manuel | 1204415 | juanconstenla@est.frba.utn.edu.ar | 
+
+## Objetivo
+
+El presente trabajo busca realizar un análisis detallado del funcionamiento de un sistema de control de riego, para el manejo automático del agua en una planta dentro en un entorno doméstico. A partir de la medición y análisis de dos variables principales, la temperatura ambiente y la humedad en tierra, se controla una bomba de riego que permite mantener la humedad dentro de los umbrales deseados, aumentando así la eficiencia del sistema y disminuyendo el estrés hídrico de la planta.
+
+El foco estará puesto en un sistema doméstico, de pequeña escala, que permita controlar el riego en una maceta; aunque los principios, materiales y tecnología aquí presentados permiten extrapolar el sistema a uno de mayores dimensiones (huerta urbana, vivero, granja, etc.) sin diferencias significativas.
+
+La variable que se controlará es la humedad de la maceta teniendo también en cuenta la temperatura ambiente, por lo tanto las señales de entrada serán la temperatura ambiente (señal digital) y la humedad en tierra (señal analógica), esta última deberá ser convertida a una señal digital en el microcontrolador.
+La realimentación será negativa y será la humedad medida en tierra. Al centrarnos en un sistema para uso doméstico no tendremos en cuenta perturbaciones que
+podrían ser significativas en un ambiente de mayor escala, como podrían ser, cantidad de luz
+solar a la que está expuesta la plantación, el radio de dispersión del agua regada, corrientes de
+aire circulantes, etc.
+
+## Circuito
+
+![Circuito](imagenes/circuito.png)
+
+## Componentes
+
+Se armó una implementación piloto utilizando los siguientes componentes:
+
+* Sensor de temperatura ambiente DHT22
+* Sensor de humedad en tierra capacitivo
+* Mini bomba de agua sumergible + diodo (1N4004)
+* Led blanco + resistencia (10 ohm)
+* Transistor (2N2222) o Mosfet  + resistencia (1k ohm)
+* Protoboard + cables + alimentación
+
+## Implementación
+
+![Implementacion](imagenes/implementacion.jpg)
+
+## Código
+
+El código desarrollado para esta prueba piloto fue escrito en Arduino IDE (lenguaje basado en C/C++) y puede encontrarse en [aquí](codigo/codigo.ino).
+
+## Conexiones
+
+### NodeMCU
+
+* Pin GND NodeMCU -> pin GND de fuente
+* Pin VIN NodeMCU -> pin VCC (5v) de fuente
+
+### Sensor de temperatura
+
+* Pin VCC (3.3v) de fuente -> pin VCC sensor de temperatura
+* Pin D2 NodeMCU -> pin DATA sensor de temperatura
+* Pin GND de fuente -> pin GND sensor de temperatura
+
+### Sensor de humedad
+
+* Pin VCC (3.3v) de fuente -> pin VCC sensor de humedad
+* Pin A0 NodeMCU -> pin DATA sensor de humedad
+* Pin GND de fuente -> pin GND sensor de humedad
+
+### Led de status
+
+* Pin D1 NodeMCU -> Ánodo (+) LED de status
+* Cátodo (-) LED de status -> resistencia
+* Resistencia -> Pin GND de fuente
+
+### Transistor
+
+* Pin D6 NodeMCU -> resistencia
+* Resistencia -> pin BASE de transistor
+* Pin EMISOR de transistor -> pin GND de fuente
+
+### Bomba
+
+* Pin VCC de bomba -> pin VCC (5v) de fuente
+* Pin GND de bomba -> pin COLECTOR de transistor
+
+### Diodo
+
+* Cátodo (-) de diodo -> pin VCC (5v) de fuente
+* Ánodo (+) de diodo -> pin COLECTOR de transistor
---
+++ codigo/codigo.ino
@@ -0,0 +1,132 @@
+#include "DHT.h"
+
+#define LEDSTATUS 5  // led de status conectado al pin digital D1 (GPIO5)
+#define PINTEMPERATURA 4  // sensor de temperatura conectado al pin digital D2 (GPIO4)
+#define PINBOMBA 12 // bomba de agua conectada al pin digital D6 (GPIO12)
+#define PINHUMEDAD 0  // sensor de humedad conectado al pin analógico A0
+#define INTERVALO 60  // intervalo entre mediciones cuando NO hay que regar (segundos)
+#define CONSTANTECAUDAL 50 // constante experimental según la potencia de la bomba y la superficie de riego
+#define CONSTANTEABSORCION 10 // constante experimental según el tamaño de la maceta y la evotranspiración de la planta
+
+DHT dht(PINTEMPERATURA, DHT22);  // inicializo sensor de temperatura ambiente
+
+const int humedadAire = 840;  // valor de humedad calibrado en aire
+const int humedadAgua = 470;  // valor de humedad calibrado en agua
+int rangoHumedad = humedadAire - humedadAgua;  // rango de humedad calibrado
+
+float temperaturaMedida = 0;
+int humedadMedida = 0;  // valor medido de resistividad, inversamente proporcional a la humedad absoluta
+int humedadMedidaAbsoluta = 0;  // valor medido de humedad absoluta
+int humedadMedidaRelativa = 0;  // valor medido de humedad relativa (porcentaje entre humedadAire y humedadAgua)
+int humedadDeseadaRelativa =  80;  // valor esperado de humedad en % siendo 0% = seco (humedadAire) y 100% = humedadAgua)
+int diferenciaHumedad = 0;  // valor de error entre humedad medida y humedad deseada
+
+int proximaMedicion = 0;
+int duracionBomba = 0;
+
+void esperarTiempo(int tiempo){
+  Serial.print("Esperando por ");
+  Serial.print(tiempo);
+  Serial.print(" segundos hasta la próxima medición...");
+
+  int tiempoTranscurrido = 0;
+  while (tiempoTranscurrido < tiempo){
+    Serial.print(".");
+    delay(1000);
+    tiempoTranscurrido++;
+  } 
+}
+
+void setup() {
+  pinMode(LEDSTATUS, OUTPUT);
+  pinMode(PINTEMPERATURA, INPUT);
+  pinMode(PINBOMBA, OUTPUT);
+
+  Serial.begin(9600);  // abro puerto serial con tasa 9600 bps
+  
+  Serial.println("");
+  Serial.println("");
+  Serial.println("    << Sistema de Riego >>    ");
+  Serial.println("  Teoría de Control - UTN - FRBA  ");
+  Serial.println("Nahuel Oyhanarte <noyhanarte@gmail.com>");
+  Serial.println("Hernán Domingo <hernan.domingo.22@gmail.com>");
+  Serial.println("Rodrigo De Luca <cdelucagallego@est.frba.utn.edu.ar>");
+  Serial.println("Juan Manuel Constenla <juanconstenla@est.frba.utn.edu.ar>");
+
+  Serial.println("");
+  
+  dht.begin();
+  delay(2000);
+}
+
+void loop() {
+  Serial.println("");
+  digitalWrite(LEDSTATUS, HIGH);
+  
+  // mido temperatura ambiente
+  temperaturaMedida = dht.readTemperature();
+
+  // registro temperatura ambiente
+  if (!isnan(temperaturaMedida)) {
+    Serial.print(F("Temperatura: "));
+    Serial.print(temperaturaMedida);
+    Serial.println(F("°C"));
+  } else {
+    Serial.println(F("Error al leer del sensor de temperatura."));
+    temperaturaMedida = 20;  // valor default en caso de error de medición
+  }  
+
+  // mido humedad en suelo
+  humedadMedida = analogRead(PINHUMEDAD); 
+  humedadMedidaAbsoluta = 1023 - humedadMedida;
+  humedadMedidaRelativa = (humedadMedidaAbsoluta - (1023 - humedadAire)) * 100 / rangoHumedad;
+
+  // registro humedad en suelo
+  Serial.print("Humedad medida absoluta: ");
+  Serial.println(humedadMedidaAbsoluta);  
+  Serial.print("Humedad medida relativa: ");
+  Serial.println(humedadMedidaRelativa);
+  Serial.print("Humedad deseada relativa: ");
+  Serial.println(humedadDeseadaRelativa);
+
+  // calculo señal de error
+  diferenciaHumedad = humedadDeseadaRelativa - humedadMedidaRelativa;
+
+  if (diferenciaHumedad > 0) {
+    Serial.println("Indicación: Hay q regar!");
+    
+    // calculo duración de accionamiento de la bomba (segundos)
+    duracionBomba = diferenciaHumedad * temperaturaMedida / CONSTANTECAUDAL;
+
+    // activo la bomba (delay requiere milisegundos)
+    Serial.print("Regando por ");
+    Serial.print(duracionBomba);
+    Serial.print(" segundos...");
+    
+    digitalWrite(PINBOMBA, HIGH);
+
+    int tiempoRegado = 0;
+    while (tiempoRegado < duracionBomba){
+      Serial.print(".");
+      delay(1000);
+      tiempoRegado++;
+    }
+    Serial.println("");
+
+    digitalWrite(PINBOMBA, LOW);
+    
+    // calculo tiempo hasta la próxima medición
+    proximaMedicion = duracionBomba * CONSTANTEABSORCION;
+    
+    // espero hasta la próxima medición (delay requiere milisegundos)
+    esperarTiempo(proximaMedicion);
+      
+  } else {
+    Serial.println("Indicación: NO hay que regar."); 
+    
+    // espero intervalo pre-seteado antes de volver a chequear (delay requiere milisegundos)
+    esperarTiempo(INTERVALO);
+  
+  }
+  Serial.println("");
+}
